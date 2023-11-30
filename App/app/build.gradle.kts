45ea9d918a57f2aacb21f9191ab95bba88e7f46c
owenheyrman <owen.heyrman@student.ap.be>
2023-11-30 12:10:40 +0100

    add firebase ui auth

--- App/app/build.gradle.kts
+++ App/app/build.gradle.kts
@@ -51,4 +51,5 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation(platform("com.google.firebase:firebase-bom:32.6.0"))
+    implementation("com.firebaseui:firebase-ui-auth:7.2.0")
 }
