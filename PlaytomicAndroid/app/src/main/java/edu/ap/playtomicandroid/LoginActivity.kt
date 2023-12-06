8bdbe1db9ced08a2b04fa906c771b0af19f32ec9
owenheyrman <owen.heyrman@student.ap.be>
2023-12-06 16:26:24 +0100

    add register/login functionality

--- PlaytomicAndroid/app/src/main/java/edu/ap/playtomicandroid/LoginActivity.kt
+++ PlaytomicAndroid/app/src/main/java/edu/ap/playtomicandroid/LoginActivity.kt
@@ -8,39 +8,39 @@ import android.widget.EditText
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 
-class RegisterActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
+    private lateinit var  auth: FirebaseAuth
 
-    private lateinit var auth: FirebaseAuth
     private lateinit var editTextEmailAddress: EditText
     private lateinit var editTextPassword: EditText
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        setContentView(R.layout.activity_login)
 
-        auth = FirebaseAuth.getInstance()
-
+        auth= FirebaseAuth.getInstance()
 
         editTextEmailAddress = findViewById(R.id.editTextEmailAddress)
         editTextPassword = findViewById(R.id.editTextPassword)
     }
 
-    fun register(view: View) {
-        val email = editTextEmailAddress.text.toString()
-        val password = editTextPassword.text.toString()
+    fun login(view: View){
+        val email=editTextEmailAddress.text.toString()
+        val password=editTextPassword.text.toString()
 
-        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener { task ->
-            if (task.isSuccessful) {
-                val intent = Intent(this, MainActivity::class.java)
+        auth.signInWithEmailAndPassword(email,password).addOnCompleteListener { task ->
+            if(task.isSuccessful){
+                val intent= Intent(this,MainActivity::class.java)
                 startActivity(intent)
                 finish()
             }
         }.addOnFailureListener { exception ->
-            Toast.makeText(applicationContext, exception.localizedMessage, Toast.LENGTH_LONG).show()
+            Toast.makeText(applicationContext,exception.localizedMessage, Toast.LENGTH_LONG).show()
         }
     }
-    fun goToLogin(view: View){
-        val intent= Intent(this,LoginActivity::class.java)
+
+
+    fun goToRegister(view: View){
+        val intent= Intent(this,RegisterActivity::class.java)
         startActivity(intent)
     }
 }
