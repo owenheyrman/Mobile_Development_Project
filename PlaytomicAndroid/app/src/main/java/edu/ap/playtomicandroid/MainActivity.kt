fc033c668af57a9a47098eae111d33858943cfa0
owenheyrman <owen.heyrman@student.ap.be>
2023-12-06 14:09:43 +0100

    make navigation functional

--- PlaytomicAndroid/app/src/main/java/edu/ap/playtomicandroid/MainActivity.kt
+++ PlaytomicAndroid/app/src/main/java/edu/ap/playtomicandroid/MainActivity.kt
@@ -22,11 +22,11 @@ class MainActivity : AppCompatActivity() {
         val navView: BottomNavigationView = binding.navView
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+        // Update the IDs to match those in the mobile_navigation.xml
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+                R.id.navigation_play, R.id.navigation_discovery,
+                R.id.navigation_community, R.id.navigation_profile
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
