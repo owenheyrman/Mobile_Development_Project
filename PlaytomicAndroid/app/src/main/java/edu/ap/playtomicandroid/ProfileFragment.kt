80ded26f08f0b703ef716f3ec39e26b54fffe4ad
owenheyrman <owen.heyrman@student.ap.be>
2023-12-06 16:48:16 +0100

    Add name, edit profile, logout to profile page

--- PlaytomicAndroid/app/src/main/java/edu/ap/playtomicandroid/ProfileFragment.kt
+++ PlaytomicAndroid/app/src/main/java/edu/ap/playtomicandroid/ProfileFragment.kt
@@ -1,59 +1,61 @@
 package edu.ap.playtomicandroid
 
+import android.app.AlertDialog
 import android.os.Bundle
+import android.text.InputType
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ProfileFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ProfileFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var textViewName: TextView
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ProfileFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ProfileFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        textViewName = view.findViewById(R.id.textViewName)
+        val buttonEditProfile = view.findViewById<Button>(R.id.buttonEditProfile)
+
+        buttonEditProfile.setOnClickListener {
+            showEditUsernameDialog()
+        }
+    }
+
+    private fun showEditUsernameDialog() {
+        // Example: Using an AlertDialog to get user input
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setTitle("Edit Username")
+
+        val input = EditText(requireContext())
+        input.inputType = InputType.TYPE_CLASS_TEXT
+        builder.setView(input)
+
+        builder.setPositiveButton("OK") { dialog, _ ->
+            val newUsername = input.text.toString()
+            updateUsername(newUsername)
+            dialog.dismiss()
+        }
+
+        builder.setNegativeButton("Cancel") { dialog, _ -> dialog.cancel() }
+
+        builder.show()
+    }
+
+    private fun updateUsername(username: String) {
+        textViewName.text = username
+        // Here you can also save the username to a database or shared preferences
     }
 }
+
